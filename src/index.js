1981e8d73321089e850d4cc738202528ea46564d
cun1crusher <superhanty@gmail.com>
2021-02-09 18:52:43 +0300

    feat: add min max avg feature

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (arguments.length === 0 || array.length === 0) {
+    return 0;
+  }
+  else {
+    let rec = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (rec > array[i]) {
+        rec = array[i];
+      }
+    }
+    return rec;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (arguments.length === 0 || array.length === 0) {
+    return 0;
+  }
+  else {
+    let rec = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (rec < array[i]) {
+        rec = array[i];
+      }
+    }
+    return rec;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (arguments.length === 0 || array.length === 0) {
+    return 0;
+  }
+  else {
+    let avg = 0;
+    let rec = [];
+    for (let i = 0; i < array.length; i++) {
+      rec = + rec + array[i];
+    }
+    avg = rec / array.length;
+    return avg;
+  }
 }
